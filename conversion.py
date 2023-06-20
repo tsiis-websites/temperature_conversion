5e93b28f6d7f4afa9574cd933cfe450e8a942c32
gerben kaas <info@tsiis.com>
2023-06-20 12:23:10 +0200

    fixes typos in celsius_to_fahrenheit

--- conversion.py
+++ conversion.py
@@ -1,5 +1,5 @@
-def celcius_to_farenheit(celcius):
-    return celcius/5*9 + 32
+def celsius_to_farenheit(celsius):
+    return celsius/5*9 + 32
 
 def celsius_to_kelvin(celsius):
     return celsius + 273.15
