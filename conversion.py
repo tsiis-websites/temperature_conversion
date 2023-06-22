e0ff0db50a50221b4afea89946af0ea68704ff5a
tsiis-websites <info@tsiis.com>
2023-06-22 10:01:50 +0200

    add first test

--- conversion.py
+++ conversion.py
@@ -3,9 +3,11 @@ def celsius_to_fahrenheit(celsius):
     return celsius/5*9 + 32
 
 def celsius_to_kelvin(celsius):
+    """This function converts Celsius to Kelvin, it takes a temperature in Celsius as an argument."""
     return celsius + 273.15
 
 def kelvin_to_fahrenheit(kelvin):
+    """This function converts Kelvin to Fahrenheit, it takes a temperature in Kelvin as an argument."""
     celsius = kelvin - 273.15
     return  celsius_to_fahrenheit(celsius)
 
---
+++ test_conversion.py
@@ -0,0 +1,4 @@
+from conversion import celsius_to_fahrenheit
+
+def test_celsius_to_fahrenheit():
+    assert celsius_to_fahrenheit(20) == 68
